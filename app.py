1340c4926bfec11d850e92ad343972a9cb6139df
sagodi97 <36556883+sagodi97@users.noreply.github.com>
2018-12-03 02:20:22 +0100

    Add requirements.txt

--- app.py
+++ app.py
@@ -14,9 +14,29 @@ def get_movie_detail():
     api_key = os.getenv('OMDB_API_KEY')
     movie_detail = requests.get('http://www.omdbapi.com/?t={0}&apikey={1}'.format(movie, api_key)).content
     movie_detail = json.loads(movie_detail)
-    response = F"""Check it out:\n\n
-{movie_detail['Title']}({movie_detail['Released']})\n
-{movie_detail['Plot']}\n\nCast : { movie_detail['Actors']}\n """
+    if movie_detail['Response'] == 'True' and movie_detail['Type'] == 'series':
+        response = F"""Check it out:\n\n{movie_detail['Title']} ({movie_detail['Released']})\n\n{movie_detail['Plot']}\n
+Cast : { movie_detail['Actors']}\n\nIMDB Rating: {movie_detail['Ratings'][0]['Value']}\nNumber of Seasons: {movie_detail['totalSeasons']}"""
+    elif movie_detail['Response'] == 'True' and movie_detail['Type'] == 'movie':
+        response = F"""Check it out:\n\n                                        
+        {movie_detail['Title']} ({movie_detail['Released']})\n                  
+        {movie_detail['Plot']}\n\nCast : { movie_detail['Actors']}\n            
+                                                                                
+        Rotten Tomatoes Tomatometer: 🍅 {movie_detail['Ratings'][1]['Value']}    
+        IMDB Rating: {movie_detail['Ratings'][0]['Value']}\n                    
+        Metacritic Score: {movie_detail['Ratings'][2]['Value']}"""
+    elif movie_detail['Response'] == 'False':
+        response = f"Sorry, I looked for {movie} in my brain but nothing came up..."
+
+
+
+
+
+
+
+
+
+
     reply = {
         'fulfillmentText': response
     }
